1ee4c00fa37cd2124ede698b06c9d8f90d8a0f9b
codeninja17 <ankitkr09@gmail.com>
2021-06-10 20:35:24 +0530

    Allocate order service

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-09T22:18:01+0530",
+    date = "2021-06-09T23:41:43+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.10 (AdoptOpenJDK)"
 )
 @Component
